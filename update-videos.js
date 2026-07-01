891aded0efbc0267ce4797433bf2ee3f3dd20c11
pint-sys <pintgamer94@gmail.com>
2026-07-01 01:15:13 -0400

    Delete update-videos.js

--- update-videos.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-const fs = require('fs');
-
-const CHANNELS = [];
-
-const GTA6_KEYWORDS = ['gta 6','gta vi','grand theft auto 6','grand theft auto vi','gta6','gtavi'];
-
-// Leave this completely empty so no backup videos are loaded
-const FALLBACK = [];
